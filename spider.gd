ab8e592917df8c76da8f2c7472f498dd0b21577d
Aldair2005 <aldairori2005@gmail.com>
2023-11-16 14:14:17 +0100

    fc

--- spider.gd
+++ spider.gd
@@ -1,19 +1,23 @@
 extends CharacterBody2D
 
 var direccion= Vector2(0,0)
-const  maxV = 500
-const velocidadH = 200
-const velocidadV = 200
+const  maxV = 100
+const velocidadH = 50
+const velocidadV = 50
 
 @onready var sprite2D = $Sprite2D
 @onready var animationPlayer = $AnimationPlayer
+
+func _ready():
+	var _sprite2D = $Sprite2D
+	var _animationPlayer = $AnimationPlayer
 
 func _physics_process(_delta):
 	mover()
 	avanzar()
 	move_and_slide()
 	
-func mover():	
+func mover():
 	if Input. is_action_just_pressed("derechaButton"):
 		direccion.x = velocidadH
 		sprite2D.flip_h= false
@@ -22,7 +26,7 @@ func mover():
 	elif Input.is_action_just_pressed("izquierdaButton"):
 		direccion.x = -velocidadH
 		sprite2D.flip_h= false
-		animationPlayer.play()
+		animationPlayer.play("retroceder")
 		direccion.x = max(direccion.x-velocidadH,-maxV)
 	else:
 		animationPlayer.play("RESET")
@@ -38,7 +42,7 @@ func avanzar():
 	elif Input. is_action_pressed("abajoButton"):
 		direccion.y = -velocidadV
 		sprite2D.flip_h= false
-		animationPlayer.play()
+		animationPlayer.play("retroceder")
 		direccion.y = max(direccion.y-velocidadV,-maxV)
 	else:
 		direccion.y += 0
