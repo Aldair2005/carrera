677a9d594e76a5ff0225402b502dea12dd2e7c42
Aldair2005 <aldairori2005@gmail.com>
2023-11-17 00:22:19 +0100

    fc

--- nodo.gd
+++ nodo.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+@onready var span = $span
 
+var spider = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spider = get_tree().get_first_node_in_group("spider") 
+	assert(spider!=null)# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
--- spider.gd
+++ spider.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-var direccion= Vector2(0,0)
-@export var  maxV = 100
-@export var velocidadH = 50
-@export var velocidadV = 50
+
+
+@export var  maxV = 200
 
 @onready var sprite2D = $Sprite2D
 @onready var animationPlayer = $AnimationPlayer
@@ -12,37 +11,29 @@ func _ready():
 	var _sprite2D = $Sprite2D
 	var _animationPlayer = $AnimationPlayer
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	var direccion= Vector2(Input.get_axis("izquierdaButton", "derechaButton"), 
+	Input.get_axis("arribaButton", "abajoButton"))
+	velocity = direccion * maxV
 	mover()
 	avanzar()
 	move_and_slide()
 	
 func mover():
-	if Input. is_action_just_pressed("derechaButton"):
-		direccion.x = velocidadH
+	if Input. is_action_pressed("derechaButton"):		
 		sprite2D.flip_h= false
 		animationPlayer.play("adelante")
-		direccion.x = max(direccion.x+velocidadH,maxV)
-	elif Input.is_action_just_pressed("izquierdaButton"):
-		direccion.x = -velocidadH
+	elif Input.is_action_pressed("izquierdaButton"):
 		sprite2D.flip_h= false
 		animationPlayer.play("retroceder")
-		direccion.x = max(direccion.x-velocidadH,-maxV)
-	else:
-		animationPlayer.play("RESET")
-		direccion.x = 0
+	move_and_slide()
 		
 func avanzar():
 		
 	if Input. is_action_pressed("arribaButton"):
-		direccion.y = velocidadV
 		sprite2D.flip_h= false
 		animationPlayer.play("adelante")
-		direccion.y = max(direccion.y+velocidadV,maxV)
 	elif Input. is_action_pressed("abajoButton"):
-		direccion.y = -velocidadV
 		sprite2D.flip_h= false
 		animationPlayer.play("retroceder")
-		direccion.y = max(direccion.y-velocidadV,-maxV)
-	else:
-		direccion.y += 0
+	move_and_slide()
